9887ce4a4cdf4b73263b59b46f99f063d930f52b
Fahad Almutairi <88871860+iLawFD@users.noreply.github.com>
2023-02-18 11:13:19 +0300

    Update assignment3 (1).py

--- assignment3 (1).py
+++ assignment3 (1).py
@@ -5,7 +5,6 @@
 # All questions are weighted the same in this assignment. This assignment requires more individual learning then the last one did - you are encouraged to check out the [pandas documentation](http://pandas.pydata.org/pandas-docs/stable/) to find functions or methods you might not have used yet, or ask questions on [Stack Overflow](http://stackoverflow.com/) and tag them as pandas and python related. All questions are worth the same number of points except question 1 which is worth 17% of the assignment grade.
 # 
 # **Note**: Questions 3-13 rely on your question 1 answer.
-
 # In[1]:
 
 
